b8764460d0f9a03f4f388a61a626af6f4ec0fc8c
Ayako Prokopczuk <ayprokop@c2s18d119.42wolfsburg.de>
2024-03-12 17:32:04 +0100

    printf_hex.c modi

--- ft_printf_hex.c
+++ ft_printf_hex.c
@@ -6,24 +6,69 @@
 /*   By: ayprokop <ayprokop@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 15:06:30 by ayprokop          #+#    #+#             */
-/*   Updated: 2024/01/27 12:24:32 by ayprokop         ###   ########.fr       */
+/*   Updated: 2024/03/12 17:31:26 by ayprokop         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+// #include <unistd.h>
 
-int	ft_printf_hex(unsigned long n)
+
+void	ft_putchar_fd(char c, int fd)
 {
-	char	hex[(int) sizeof (unsigned long)];
-	int		count;
-
-	count = 0;
-	if (n == 0)
-	{
-		ft_putchar_fd('0', 1);
-		return (1);
-	}
-	count += ft_basetobase(n, 16, hex);
-	ft_putstr_fd(hex, 1);
-	return (count);
+	write(fd, &c, 1);
 }
+
+
+int	ft_len_of_hex(unsigned int n)
+{
+	int	len;
+
+	len = 0;
+	while (len != 0)
+	{
+		len++;
+		n = n % 16;
+	}
+	return (len);
+}
+
+void	ft_put_hex(unsigned int n, const char type)
+{
+	if (n >= 16)
+	{
+		ft_put_hex(n / 16, type);
+		ft_put_hex(n % 16, type);
+	}
+	else
+	{
+		if (n <= 9)
+			ft_putchar_fd((n + 48), 1);
+		else
+		{
+			if (type == 'x')
+				ft_putchar_fd((n - 10 + 'a'), 1);
+			if (type == 'X')
+				ft_putchar_fd((n - 10 + 'A'), 1);
+		}
+		
+	}
+	
+}
+
+int	ft_print_hex(unsigned int n, const char type)
+{
+
+	if (n == 0)
+		return (write(1, "0", 1));
+	else
+		ft_put_hex(n, type);
+	return (ft_len_of_hex(n));
+}
+
+// int	main(void)
+// {
+//     ft_print_hex(-462, 'x');
+//     printf("\nprintf hexidesimal small and negative :%x\n",-462);
+// 	return (0);
+// }
